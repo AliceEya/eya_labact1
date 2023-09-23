423277bfe83fcc5bc170fdb22d6afdee31bb866e
AliceEya <aeya_21ur1425@psu.edu.ph>
2023-09-24 01:27:26 +0800

    Added Box Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -14,65 +14,80 @@ class FirstApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.home),
-          backgroundColor: Color.fromARGB(255, 238, 197, 197),
+          backgroundColor: Color.fromARGB(255, 242, 66, 157),
+          actions: [Icon(Icons.access_time)],
           title: const Text(
-            "MY INFO",
+            "My Bio",
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.normal,
+              
             )
             ),
         ),
-        backgroundColor: Color.fromARGB(255, 126, 126, 241),
-        body: Container(
-          padding: const EdgeInsets.all(30),
-          alignment: Alignment.center,
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-               Color.fromARGB(255, 255, 189, 235),
-               Color.fromARGB(255, 239, 147, 236)
-              ]),
-          ),
-          child: const Column(
-            children: <Widget> [
-            Text(
-              "Alice Zandra Eya",
-                  style:  TextStyle(   
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
+       
+        backgroundColor:  Color.fromARGB(255, 248, 141, 244),
+     
+    body: SafeArea(
+      child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 239, 40, 123),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.white,
+                 
+                  ),
+                ),
+            
+
+                margin: EdgeInsets.all(50),
+                width: 500,
+                height: 500,
+                 
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+              Text(
+                "Alice Zandra Eya",
+                    style:  TextStyle( 
+                      decoration: TextDecoration.underline,  
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+                fontWeight: FontWeight.normal,
+                ),
               ),
-            ),
-           Text(
-            "Brgy. Salcedo, San Manuel, Tarlac",
-              style:  TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
- ),
-            ),
-   Text(
-              "BSIT 3D",
-              style:  TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
+             Text(
+              "Brgy. Salcedo, San Manuel, Tarlac",
+                style:  TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+     ),
               ),
+       Text(
+                "BSIT 3D",
+                style:  TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+                ),
+                ),
+              Text(
+                "Fake it until you make it.",
+                style:  TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+                ),
+                ),
+              ],
+            )
+          
+
               ),
-            Text(
-              "Fake it until you make it.",
-              style:  TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
-              ),
-              ),
-            ],
-          )
-            ),
+    ),
             ),
     );
   }
